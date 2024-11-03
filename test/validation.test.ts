4645c97b95f7a73a04663629a8a828338d3c5d9d
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-03 08:17:38 +0700

    Object Validation - video 85

--- test/validation.test.ts
+++ test/validation.test.ts
@@ -2,84 +2,130 @@ import {z, ZodError, ZodObject} from "zod"
 
 describe('zod', () => {
     
-    it('should support', async () => {
-        const schema = z.string().min(3).max(100)
+    // it('should support', async () => {
+    //     const schema = z.string().min(3).max(100)
         
-        const request = "eko"
-        const result = schema.parse(request)
+    //     const request = "eko"
+    //     const result = schema.parse(request)
         
-        expect(result).toBe("eko")
-    })
+    //     expect(result).toBe("eko")
+    // })
 
-    it('should support validate primitive data type', async () => {
+    // it('should support validate primitive data type', async () => {
         
-        const usenameSchema = z.string().email()
-        const isAdminSchema = z.boolean()
-        const priceSchema = z.number().min(1000).max(1000000)
+    //     const usenameSchema = z.string().email()
+    //     const isAdminSchema = z.boolean()
+    //     const priceSchema = z.number().min(1000).max(1000000)
 
-        const username = usenameSchema.parse("nabil@passmail.com")
-        console.info("Email :", username)
+    //     const username = usenameSchema.parse("nabil@passmail.com")
+    //     console.info("Email :", username)
 
-        const isAdmin = isAdminSchema.parse(true)
-        console.info("Aku adalah super hero :",isAdmin)
+    //     const isAdmin = isAdminSchema.parse(true)
+    //     console.info("Aku adalah super hero :",isAdmin)
 
-        const price = priceSchema.parse(20000)
-        console.info("Rp.", price)
-    })
+    //     const price = priceSchema.parse(20000)
+    //     console.info("Rp.", price)
+    // })
 
-    it('should support data conversion', async () => {
+    // it('should support data conversion', async () => {
         
-        const usenameSchema = z.coerce.string().min(3).max(100)
-        const isAdminSchema = z.coerce.boolean()
-        const priceSchema = z.coerce.number().min(1000).max(1000000)
+    //     const usenameSchema = z.coerce.string().min(3).max(100)
+    //     const isAdminSchema = z.coerce.boolean()
+    //     const priceSchema = z.coerce.number().min(1000).max(1000000)
 
-        const username = usenameSchema.parse(3393939)
-        console.info(username)
+    //     const username = usenameSchema.parse(3393939)
+    //     console.info(username)
 
-        const isAdmin = isAdminSchema.parse("true")
-        console.info(isAdmin)
+    //     const isAdmin = isAdminSchema.parse("true")
+    //     console.info(isAdmin)
 
-        const price = priceSchema.parse("20000")
-        console.info(price)
-    })
+    //     const price = priceSchema.parse("20000")
+    //     console.info(price)
+    // })
 
-    it('should support date validation', async () => {
-        const birthDateSchema = z.coerce.date().min(new Date(1980, 0, 1)).max(new Date(2020, 0, 1))
+    // it('should support date validation', async () => {
+    //     const birthDateSchema = z.coerce.date().min(new Date(1980, 0, 1)).max(new Date(2020, 0, 1))
 
-        const birthDate = birthDateSchema.parse("1990-01-01")
-        console.info(birthDate)
+    //     const birthDate = birthDateSchema.parse("1990-01-01")
+    //     console.info(birthDate)
         
-        const birthDate2 = birthDateSchema.parse(new Date(1990, 1, 1))
-        console.info(birthDate2)
-    })
+    //     const birthDate2 = birthDateSchema.parse(new Date(1990, 1, 1))
+    //     console.info(birthDate2)
+    // })
 
     // validation error - video 84
-    it('should support zod error if invalid', async () => {
+    // it('should support zod error if invalid', async () => {
         
-        const schema = z.string().email().min(3).max(100)
+    //     const schema = z.string().email().min(3).max(100)
 
-        try {
-            schema.parse("el")
-        } catch (err) {
-            if (err instanceof ZodError) {
-                console.error(err)
-                // err errors.forEach((error) => {
-                //     console.info(error.message)
-                // })
-            }
+    //     try {
+    //         schema.parse("el")
+    //     } catch (err) {
+    //         if (err instanceof ZodError) {
+    //             console.error(err)
+    //             // err errors.forEach((error) => {
+    //             //     console.info(error.message)
+    //             // })
+    //         }
+    //     }
+    // })
+
+    // it('should support zod error if invalid without exception', async () => {
+        
+    //     const schema = z.string().email().min(3).max(100)
+
+    //     const result = schema.safeParse("Kaoka@gmail.com")
+
+    //     if (result.success) {
+    //         console.info(result)
+    //     } else {
+    //         console.info(result.error)
+    //     }
+    // })
+
+    // object validation - video 85
+    it('should can validate object', () => {
+        const loginSchema = z.object({
+            username: z.string().email(),
+            password: z.string().min(6).max(20)
+        })
+
+        const request = {
+            username: "nabil@gmail.com",
+            password: "123456789",
+            ignore: "ignore"
         }
+
+        const result = loginSchema.parse(request)
+        console.info(result)
     })
 
-    it('should support zod error if invalid without exception', async () => {
+    it('should support nested object', async () => {
         
-        const schema = z.string().email().min(3).max(100)
+        const createUserSchema = z.object({
+            id: z.string().max(100),
+            name: z.string().max(100),
+            address: z.object({
+                street: z.string().max(100),
+                city: z.string().max(100),
+                zip: z.string().max(10),
+                country: z.string().max(100)
+            })
+        })
 
-        const result = schema.safeParse("Kaoka@gmail.com")
-
-        if (result.success) {
-            console.info(result)
-        } else {
-            console.info(result.error)
+        const request = {
+            id: "123",
+            name: "Eldo",
+            address: {
+                street: "Margacinta Raya",
+                city: "Bandung",
+                zip: "12345",
+                country: "Indonesia"
+            }
         }
+
+        const result = createUserSchema.parse(request)
+        console.info(result)
+
     })
 })
